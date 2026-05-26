6dcfe5ce09475722b7f1e2110937666767135fb9
Neerav Durve <neerav.durve@gmail.com>
2026-05-26 19:19:02 +0100

    Add files via upload

---
+++ 371_sum_of_two_ints.py
@@ -0,0 +1,10 @@
+class Solution:
+    def getSum(self, a, b):
+        ab = [a, b]
+        return sum(ab)
+
+testing = Solution()
+print(testing.getSum(1, 2))
+
+#runtime: 0ms, beats 100.00%
+#memory: 19.29MB, beats 53.76%
